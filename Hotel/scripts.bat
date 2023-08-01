98ac1d50121c5764369bca6a40707dd14fd0e2da
sxa141101 <sxa141101@darden.com>
2023-08-01 16:49:01 +0530

    dropdown added

--- Hotel/scripts.bat
+++ Hotel/scripts.bat
@@ -1,8 +1,8 @@
 @echo off
 start "api-gateway" java -jar api-gateway.jar
 start "discovery-server" java -jar discovery-server.jar
-start "Guest" java -jar guest-management.jar
+start "Guest" java -jar guest-management-0.0.1-SNAPSHOT.jar
 start "Inventory" java -jar inventory-mangement.jar
-start "Reservation" java -jar reservation-management.jar
-start "Room" java -jar room-mangement.jar
+start "Reservation" java -jar reservation-management-0.0.1-SNAPSHOT.jar
+start "Room" java -jar room-mangement-0.0.1-SNAPSHOT.jar
 start "User" java -jar user-mangement.jar
